760d42a1ccf11a1e2a6a3f673a589a1da34d6476
john <aarontu803@gmail.com>
2023-08-09 03:00:25 +0800

    modify viteConfig

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import path from 'node:path';
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import liveReload from 'vite-plugin-live-reload'
-// https://vitejs.dev/config/
+
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 export default defineConfig({
