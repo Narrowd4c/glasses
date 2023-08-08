903cc3db70bd974408504381313bd9b904070223
john <aarontu803@gmail.com>
2023-08-09 03:04:16 +0800

    modify viteConfig

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import liveReload from 'vite-plugin-live-reload'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 export default defineConfig({
-  base:'glasses',
+  base:'/glasses/',
   plugins: [
     vue(),
     liveReload(['./index.html', './src/**/*.vue'])
